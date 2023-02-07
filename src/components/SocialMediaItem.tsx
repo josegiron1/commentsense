b6f481889001c4f2c8b16b83c33b85e7649b9994
Jose Giron <62405132+josegiron1@users.noreply.github.com>
2023-02-07 07:43:54 -0400

    add underline to SocialMediaItem if hover

--- src/components/SocialMediaItem.tsx
+++ src/components/SocialMediaItem.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import React from "react";
 
 export default function SocialMediaItem({ alt, src, href, text }: { alt: string; src: string; href: string; text: string }) {
     return (
-        <a href={href} target="_blank" rel="noreferrer" className="flex sm:flex-row flex-col items-center gap-2">
+        <a href={href} target="_blank" rel="noreferrer" className="flex sm:flex-row flex-col items-center gap-2 hover:underline">
             <Image src={src} width={24} height={24} alt={alt} /> <span>{text}</span>
         </a>
     );
